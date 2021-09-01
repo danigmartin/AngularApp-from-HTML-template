defab871904a701ca10280515a05513777871855
Danigmartin <danigmartinweb@gmail.com>
2021-09-01 13:57:43 +0200

    rutas creadas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PortfolioComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'item',
+    component: ItemComponent
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: ''
+  }
+];
+
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, {useHash: true}),
+  ],
+  exports: [
+    RouterModule
+  ]
+
+})
+export class AppRoutingModule { }
